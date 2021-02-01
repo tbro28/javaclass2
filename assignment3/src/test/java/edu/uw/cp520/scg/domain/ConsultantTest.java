10c6448319f7000fc789fe15e45b766a4c4b4e4b
Tim Brown <brownti28@yahoo.com>
2021-01-31 22:58:10 -0800

    assignment 3 initial

---
+++ assignment3/src/test/java/edu/uw/cp520/scg/domain/ConsultantTest.java
@@ -0,0 +1,54 @@
+package edu.uw.cp520.scg.domain;
+
+import edu.uw.cp520.scg.util.PersonalName;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the Consultant class.
+ *
+ * @author Tim Brown
+ */
+public class ConsultantTest {
+
+    Consultant consultant;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createConsultant() {
+        PersonalName personalName = new PersonalName("Brown", "Tom", "Jack");
+        consultant = new Consultant(personalName);
+
+        assertNotNull(consultant);
+    }
+
+    @Test
+    void getNameOfConsultant() {
+        PersonalName personalName = new PersonalName("Brown", "Tom", "Jack");
+        consultant = new Consultant(personalName);
+
+        assertEquals(personalName, consultant.getName());
+    }
+
+    @Test
+    void getStringOfConsultant() {
+        PersonalName personalName = new PersonalName("Brown", "Tom", "Jack");
+        consultant = new Consultant(personalName);
+
+        assertEquals("Consultant{name=Consultant: Brown, Tom, Jack}",
+                consultant.toString());
+    }
+
+}
---
+++ assignment3/src/test/java/edu/uw/cp520/scg/domain/NonBillableAccountTest.java
@@ -0,0 +1,38 @@
+package edu.uw.cp520.scg.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the NonBillableAccount class.
+ *
+ * @author Tim Brown
+ */
+class NonBillableAccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Sick Leave", NonBillableAccount.SICK_LEAVE.getName());
+    }
+
+    @Test
+    void isBillable() {
+        assertFalse(NonBillableAccount.VACATION.isBillable());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("NonBillableAccount{name='Business development'}", NonBillableAccount.BUSINESS_DEVELOPMENT.toString());
+    }
+}
