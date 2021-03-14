7d453a556835bfe6c6c4e4ee6a56945fea83c660
Tim Brown <brownti28@yahoo.com>
2021-03-14 15:35:18 -0700

    Chance to start, db class created some more.

--- assignment7/src/test/java/persistent/DbServer.java
+++ assignment7/src/test/java/persistent/DbServer.java
@@ -1,4 +1,57 @@
 package persistent;
 
+import edu.uw.cp520.scg.domain.ClientAccount;
+import edu.uw.cp520.scg.domain.Consultant;
+import edu.uw.cp520.scg.domain.Invoice;
+import edu.uw.cp520.scg.domain.TimeCard;
+
+import java.time.Month;
+import java.util.List;
+
 public class DbServer {
+
+    List<ClientAccount> clientAccountList;
+    List<Consultant> consultantList;
+
+
+    public void addClient(ClientAccount client) {
+
+    }
+
+
+
+    public void addConsultant(Consultant consultant) {
+
+    }
+
+
+
+    public void addTimeCard(TimeCard timecard) {
+
+    }
+
+
+
+    public List<ClientAccount> getClients() {
+
+        return clientAccountList;
+    }
+
+
+
+    public List<Consultant> getConsultants() {
+
+        return consultantList;
+    }
+
+
+//Query
+    public Invoice getInvoice(ClientAccount client, int month, int year) {
+
+        return new Invoice(client, Month.of(month), year);
+    }
+
+
+
+
 }
